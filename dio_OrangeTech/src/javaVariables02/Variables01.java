d08ebdf059d8fccdab134a378debe7a8662aa7cf
mecfoliveira86 <mec.foliveira@hotmail.com>
2023-01-09 19:01:35 -0300

    adding primitive variables examples on the project

--- dio_OrangeTech/src/javaVariables02/Variables01.java
+++ dio_OrangeTech/src/javaVariables02/Variables01.java
@@ -1,5 +1,29 @@
 package javaVariables02;
 
 public class Variables01 {
+	
+	public static void main(String [] args) {
+		//variável para número inteiro: usar int
+		int numero = 9;
+		
+		//variável para número decimal: usar float
+		float numeroF = 9.56f;
+		
+		//variável para número decimal: usar double
+		double numeroD = 9.589456d;
+		
+		//variável para texto
+		String textoS = "Exemplo de texto";
+		
+		//variável para booleanos (TRUE / FALSE)
+		boolean status = true;	
+		
+		System.out.println("Número inteiro " + numero);
+		System.out.println("Número inteiro " + numeroF);
+		System.out.println("Número inteiro " + numeroD);
+		System.out.println("Número inteiro " + textoS);
+		System.out.println("Número inteiro " + status);
+		
+	}
 
 }
