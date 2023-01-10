92e432ae22e819df79a50e4afdfabd44fd9a7cbe
mecfoliveira86 <mec.foliveira@hotmail.com>
2023-01-09 22:26:22 -0300

    some note had been taken about Scope in Java

--- dio_OrangeTech/src/javaScope04/Scope01.java
+++ dio_OrangeTech/src/javaScope04/Scope01.java
@@ -1,5 +1,18 @@
 package javaScope04;
 
 public class Scope01 {
+	//Scope is a subject aligned to variable access inside Classes or methods.
+	
+	int valorNumeral = 9880;
+	
+	public void calcularNumero(int numero) {
+		if (numero > valorNumeral) {
+			valorNumeral++;
+		} else {
+			valorNumeral--;
+		}
+		
+		//the variable numero is not able to be used outside of calcularNumero method.
+	}
 
 }
