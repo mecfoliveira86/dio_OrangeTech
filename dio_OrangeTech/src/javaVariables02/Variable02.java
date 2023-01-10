bf1326aac7d026939cd509d091eddae4414ceef2
mecfoliveira86 <mec.foliveira@hotmail.com>
2023-01-09 21:26:07 -0300

    all the operators notes had been taken

---
+++ dio_OrangeTech/src/javaVariables02/Variable02.java
@@ -0,0 +1,12 @@
+package javaVariables02;
+
+public class Variable02 {
+	
+	public static void main(String [] args) {
+		
+		//Definição de uma constante, usar final e o nome da variável em maiúscula
+		final double VALOR_DE_PI = 3.141516d;
+		
+	}
+
+}
