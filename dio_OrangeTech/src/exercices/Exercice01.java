e203cb84df7eac0da745a4ecff05b4c7deb55e2b
mecfoliveira86 <mec.foliveira@hotmail.com>
2023-01-11 23:12:33 -0300

    performing some exercices

---
+++ dio_OrangeTech/src/exercices/Exercice01.java
@@ -0,0 +1,27 @@
+package exercices;
+
+import java.util.Scanner;
+
+/*Faça um programa que peça uma nota, entre 10 e 0.
+ * Mostre uma mensagem se o valor informado for inválido e continue pedindo
+ * até que o usuário insira um valor válido.
+ * */
+public class Exercice01 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+				
+		while(flag) {
+			System.out.println("Digite o nome do aluno: ");
+			String nome = sc.next();
+			if (nome.equals("0")) {
+				flag = false;
+				break;
+			} else {
+				System.out.println("Digite a idade do aluno: ");
+				int idade = sc.nextInt();
+			}
+		}
+		System.out.println("Finalizado!");
+	}
+}
---
+++ dio_OrangeTech/src/exercices/Exercice02.java
@@ -0,0 +1,27 @@
+package exercices;
+
+import java.util.Scanner;
+
+/*Faça um programa que leia conjuntos de dois valores,
+ * o primeiro representando o nome de um aluno e o segundo representando a sua idade.
+ * (pare inserindo o valor 0 no campo).
+ * */
+public class Exercice02 {
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int nota;
+		
+		while(true) {
+			System.out.println("Insira uma nota entre 0 e 10:");
+			nota = sc.nextInt();
+			
+			if(nota >= 0 && nota <= 10){
+				break;
+			} else {
+				System.out.println("Nota inválida!");
+			}
+		}
+	}
+
+}
