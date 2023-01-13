d1f2bbdefcc65ca62553440bbae1cfbae2aaa3e5
mecfoliveira86 <mec.foliveira@hotmail.com>
2023-01-12 23:57:48 -0300

    testing some examples

---
+++ dio_OrangeTech/src/exercices/Exercice11.java
@@ -0,0 +1,12 @@
+package exercices;
+
+public class Exercice11 {
+	public static void main (String[] args) {
+		int i, num = 5;
+        for (i = 0; i < 3; i++) {
+            num += i;
+            System.out.print(i);
+        }
+	}
+
+}
